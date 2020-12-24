9ae52c7735becc99596ffc8f80de379cbc3c78c9
Михаил Бронников <mihailbronnikov@MacBook-Pro-Mihail.local>
2020-12-24 16:32:50 +0300

    Updates

---
+++ precache-manifest.ad15c57500517c57567a6ddb4f678fee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6648140ffbc852f6e82b58d1b0755b",
+    "url": "/Social-network/index.html"
+  },
+  {
+    "revision": "5fb271da5de0a175922f",
+    "url": "/Social-network/static/css/main.7d4c4b21.chunk.css"
+  },
+  {
+    "revision": "4444b2aca5a69d7323e1",
+    "url": "/Social-network/static/js/2.06811c38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-network/static/js/2.06811c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb271da5de0a175922f",
+    "url": "/Social-network/static/js/main.ad54fb8f.chunk.js"
+  },
+  {
+    "revision": "0525d55b9514301ffb1a74f1b5946a69",
+    "url": "/Social-network/static/js/main.ad54fb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3267ed3e020fa3499c92",
+    "url": "/Social-network/static/js/runtime-main.1a3c85b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
